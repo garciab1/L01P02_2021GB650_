194c419791f4dc8306734f817c39dd8dd0f93dfe
Esme Garcìa <esmeralda.garcia1@catolica.edu.sv>
2024-04-06 08:45:51 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L01P02_2021GB650_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/notasContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace L01P02_2021GB650_.Models
+{
+    public class notasContext : DbContext
+    {
+    }
+}
